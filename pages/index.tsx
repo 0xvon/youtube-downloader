04fa1b036f7cecd6869dcad688702468334d7615
masatsch <kingmasatojames@gmail.com>
2021-09-06 12:12:59 +0900

    use next link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
@@ -18,7 +19,7 @@ const Home: NextPage = () => {
             <main className={styles.main}>
                 <h1 className={styles.title}>Welcome to CMS Templates!</h1>
                 <p>
-                    <a href="/posts">go to posts</a>
+                    <Link href="/posts">go to posts</Link>
                 </p>
             </main>
 
